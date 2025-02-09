d84c795f2bce6c80e62d021f51c95010c48bf3f9
NguyenKhanhDuy <duy01657853612@gmail.com>
2025-02-09 13:22:17 +0700

    set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center text-blue-500">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
