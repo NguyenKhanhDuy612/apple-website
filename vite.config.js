a7b7cac6c84a1544b0e17985d492ea3b2c378804
NguyenKhanhDuy <duy01657853612@gmail.com>
2025-02-24 17:46:55 +0700

    deploy v1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(),tailwindcss(),],
   server: {
     host: '0.0.0.0',  // This allows access from any device on the same network
